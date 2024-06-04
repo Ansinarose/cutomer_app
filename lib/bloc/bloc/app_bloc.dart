55bf7ee3921b1a789e06b3c7426c47f8e998745d
Ansinarose <iansinarose@gmail.com>
2024-06-04 12:23:31 +0530

    splash using bloc completed

---
+++ lib/bloc/bloc/app_bloc.dart
@@ -0,0 +1,16 @@
+import 'package:customer_application/bloc/bloc/app_event.dart';
+import 'package:customer_application/bloc/bloc/app_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class splashBloc extends Bloc<SplashEvent, splashState> {
+  splashBloc() : super(splashInitial()){
+    on<StartSplash>(_onstartSplash);
+  }
+
+  Future<void> _onstartSplash(StartSplash event,Emitter<splashState> emit) async {
+    emit(splashLoading());
+    await Future.delayed(Duration(seconds: 3));
+    emit(splashLoaded());
+  }
+}
